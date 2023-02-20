5c8faba2c40bda26e62692d29cd6cc4dd29dc890
thanosoncode <thanosoncode@gmail.com>
2023-02-20 15:51:02 +0200

    axios baseURL

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: process.env.PROD ? 'https://workouts-api-fzz9.onrender.com/api' : process.env.VITE_REACT_BASE_URL
+  baseURL: 'https://workouts-api-fzz9.onrender.com/api'
 });
 
 export default axiosInstance;
